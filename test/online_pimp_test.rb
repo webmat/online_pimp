ef825a83d6905d9c91f1eedeefa63f7213a4202b
Mathieu Martin <webmat@gmail.com>
2009-03-10 14:48:49 -0400

    tests

--- test/online_pimp_test.rb
+++ test/online_pimp_test.rb
@@ -1,7 +1,24 @@
 require File.dirname(__FILE__) + '/test_helper'
 
 class OnlinePimpTest < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  context "length_of_longest" do
+    should "return -1 when the array is empty" do
+      assert_equal -1, OnlinePimp::Internal.length_of_longest([])
+    end
+
+    should "return 4 when the longest item is 4 long" do
+      assert_equal 4, OnlinePimp::Internal.length_of_longest([ 'adsf', 'd' ])
+    end
+  end
+
+  context "display()" do
+    should "just return a string" do
+      assert_kind_of String, OnlinePimp.display()
+    end
+
+    should "display stuff properly" do
+      assert_equal "twitter.com/webmat : taken\n", 
+        OnlinePimp.display('twitter.com/webmat' => false)
+    end
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,7 @@ require 'test/unit'
 require 'shoulda'
 require 'mocha'
 
-$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'online_pimp'
 
 class Test::Unit::TestCase
